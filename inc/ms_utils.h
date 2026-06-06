f8c11b6d9b6aa8471484bf27ddaeafb7edb0b059
Ignatz <iggy@m3lt.work>
2026-06-07 01:17:09 +0200

    add idea for parsing function based on state machines

--- inc/ms_utils.h
+++ inc/ms_utils.h
@@ -12,8 +12,31 @@
 
 #ifndef MS_UTILS_H
 # define MS_UTILS_H
+# include <stddef.h>
 
 // ms_exit_if(ft_malloc(size), "malloc failed");
-void	*ms_exit_if(void *cond, const char *error_msg);
+void					*ms_exit_if(void *cond, const char *error_msg);
+
+enum					e_iter_res
+{
+	YES,
+	NO,
+	NXT
+};
+
+typedef struct s_stm_change
+{
+	enum e_iter_res		res;
+	struct s_stm_change	(*next_decision)(int c);
+}						t_stm_change;
+
+typedef t_stm_change	(*t_stm_dec)(int c);
+
+// eine funktion entscheided ob der das aktuell teil des sets ist
+// oder was die nächste aktion ist um es raus zu finden.
+// return types: yes, no, {:more_work, next_fn}
+// ms_stm returns a positive number if statemachines finds a match,
+// otherwise returns 0
+size_t					ms_stm(char *data, t_stm_change *entry);
 
 #endif
