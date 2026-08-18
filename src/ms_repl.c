9963da7eac80b514e5c106e2efbdaeec25492eb5
Felix Krüger <fkruger@students.42vienna.com>
2026-08-18 23:49:35 +0200

    wip on repl

--- src/ms_repl.c
+++ src/ms_repl.c
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ms_exec.h"
+#include "ms_parsing.h"
+#include "ms_utils.h"
+#include "ms_env.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <libft_char.h>
 
+char *ms_readline(void)
+{
+	char *line = readline("minishell> ");
+	ms_exit_if(line, "exit");
+	if(line[0] != '\0' && line[0] != ' ')
+		add_history(line);
+	return line;
+}
+
+
+void ms_repl(void)
+{
+	char *line;
+	t_ms_parse_res *parsing_result;
+
+	while(true)
+	{
+		line = ms_readline();
+		parsing_result = ms_parse(line);
+		if(!parsing_result->success)
+		{}
+	}
+}
